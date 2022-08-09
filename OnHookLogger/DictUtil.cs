0ad6dd15b48cd3fd196b1d2413845a3780b5a90a
KaanGaming <kaangaming.gamejolt@gmail.com>
2022-08-09 21:11:28 +0300

    add dictutil

---
+++ OnHookLogger/DictUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnHookLogger
+{
+	public static class DictUtil
+	{
+		public static T2? GetSafe<T1, T2>(this Dictionary<T1, T2> dict, T1 key) where T2 : struct
+		{
+			return dict.ContainsKey(key) ? dict[key] : null;
+		}
+	}
+}
