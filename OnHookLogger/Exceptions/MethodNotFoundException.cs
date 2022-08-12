44996334224033ab2b3ae75297c966bacd87a6b2
KaanGaming <kaangaming.gamejolt@gmail.com>
2022-08-12 18:08:44 +0300

    add exceptions

---
+++ OnHookLogger/Exceptions/MethodNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnHookLogger.Exceptions
+{
+	public class MethodNotFoundException : Exception
+	{
+		public MethodNotFoundException(string message) : base(message)
+		{
+
+		}
+	}
+}
---
+++ OnHookLogger/Exceptions/TypeNotFinalizedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnHookLogger.Exceptions
+{
+	public class TypeNotFinalizedException : Exception
+	{
+		public TypeNotFinalizedException(string message) : base(message)
+		{
+
+		}
+	}
+}
